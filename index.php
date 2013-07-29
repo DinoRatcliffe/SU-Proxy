4d6ec10bc3db1e7815a5b3493701dc00cf04e4c6
Dino Ratcliffe <yo@dino.io>
2013-07-29 10:38:56 +0100

    Added error message for when no url is given

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
 $ch = curl_init();
-$pageurl = $_GET['url'];
+if (isset($_GET['url'])) {
+	$pageurl = $_GET['url'];
+} else {
+	echo "ERROR: no url given";
+	exit(0);
+}
+
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
 curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_0 like Mac OS X; en-us) AppleWebKit/528.18 (KHTML, like Gecko) Version/4.0 Mobile/7A341 Safari/528.16');
