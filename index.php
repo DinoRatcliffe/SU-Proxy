0ce7b666928de89ea07075d6ce8da8ccccc243b3
Dino Ratcliffe <yo@dino.io>
2013-07-29 10:47:08 +0100

    Added ability to specify useragent

--- index.php
+++ index.php
@@ -7,9 +7,15 @@ if (isset($_GET['url'])) {
 	exit(0);
 }
 
+if (isset($_GET['ua']) && $_GET['ua'] !== "") {
+	$userAgent = $_GET['ua'];
+} else {
+	$userAgent = 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_0 like Mac OS X; en-us) AppleWebKit/528.18 (KHTML, like Gecko) Version/4.0 Mobile/7A341 Safari/528.16';
+}
+
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
-curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_0 like Mac OS X; en-us) AppleWebKit/528.18 (KHTML, like Gecko) Version/4.0 Mobile/7A341 Safari/528.16');
+curl_setopt($ch,CURLOPT_USERAGENT,$userAgent);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 curl_setopt ($ch, CURLOPT_URL, $pageurl );
 $html = curl_exec ( $ch );
@@ -24,8 +30,11 @@ $urlrootesc = str_replace(".", "\.", $urlroot);
 $html = str_replace ("href=\"(?!http)", "href=\"".$urlroot, $html);
 $html = preg_replace ("#src=\"(?!http)#", "src=\"".$urlroot, $html);
 
-$html = str_replace ("href=\"http", "href=\"/?url=http", $html);
+$html = str_replace ("href=\"http", "href=\"/?ua=".$userAgent."&url=http", $html);
 
-
-echo $html;
+if ($html == "") {
+	echo "ERROR: no site found";
+} else {
+	echo $html;
+}
 
